f57a2e2bdd3508659a98cf3ec2bf2616515f2bc8
RunpengLuo <runpeng.luo@princeton.edu>
2025-08-01 01:17:31 -0400

    finish pipeline

--- pipeline.sh
+++ pipeline.sh
@@ -1,11 +1,151 @@
 #!/bin/bash
 
-SAMPLE=
-MSR=
-MSPB=
+set -euo pipefail
 
+# input
+SAMPLE=
+OUTDIR=
+TMPDIR=
+LOGDIR=${TMPDIR}/logs
 NORMAL_BAM=
 TUMOR_BAM=
 
-region_bed=
+# parameters
+MSR=
+MSPB=
+q=
+Q=
+d=
+CHROMS=$(seq 1 22)
 
+MAXJOBS=4
+
+REGION_BED=
+DB_SNP=
+REFERENCE=
+
+mkdir -p ${OUTDIR}
+mkdir -p ${TMPDIR}
+mkdir -p ${LOGDIR}
+
+########################################
+echo "start genotyping on normal sample"
+snp_dir="${OUTDIR}/snps"
+mkdir -p ${snp_dir}
+for CHR in $CHROMS; do
+    CHROM=chr${CHR}
+    snp_file="${snp_dir}/${CHROM}.vcf.gz"
+    if [[ -f ${snp_file} ]]; then
+        echo "${CHROM} exists"
+        continue
+    fi
+    tgt_file="${TMPDIR}/target.${CHROM}.pos.gz"
+    bcftools query -f '%CHROM\t%POS\n' -r "${CHROM}" "${DB_SNP}" | gzip -9 > ${tgt_file}
+    
+    bcftools mpileup "${NORMAL_BAM}" -f "${REFERENCE}" \
+    -Ou -a INFO/AD,AD,DP --skip-indels \
+    -q ${q} -Q ${Q} -d ${d} -T "${tgt_file}" | \
+    bcftools call -m -Ou | \
+    bcftools view -v snps -g het -m2 -M2 \
+        -i "FMT/DP>=${mincov}" -Oz -o ${snp_file} &>"${LOGDIR}/genotype.${CHROM}.log" &
+
+    while [[ $(jobs -r -p | wc -l) -ge $MAXJOBS ]]; do
+        sleep 10
+    done
+done
+wait
+
+echo "allele counting on normal sample"
+for CHR in $CHROMS; do
+    CHROM=chr${CHR}
+    snp_file="${snp_dir}/${CHROM}.vcf.gz"
+    if [[ ! -f ${snp_file} ]]; then
+        exit 1
+    fi
+    tgt_file="${TMPDIR}/normal.${CHROM}.pos.gz"
+    bcftools query -f '%CHROM\t%POS\n' -r "${CHROM}" "${snp_file}" | gzip -9 > ${tgt_file}
+
+    normal_1bed="${TMPDIR}/normal.${CHROM}.1bed"
+    bcftools query -f '%CHROM\t%POS\tnormal\t[%AD{0}\t%AD{1}]\n' \
+		"${vcf_file_final}" \
+		-o "${normal_1bed}"
+done
+
+echo "allele counting on ${SAMPLE}"
+for CHR in $CHROMS; do
+    CHROM=chr${CHR}
+    tumor_1bed="${TMPDIR}/${SAMPLE}.${CHROM}.1bed"
+    if [[ -f ${tumor_1bed} ]]; then
+        echo "${CHROM} exists"
+        continue
+    fi
+    tgt_file="${TMPDIR}/normal.${CHROM}.pos.gz"
+	bcftools mpileup "${TUMOR_BAM}" -f "${REFERENCE}" \
+		-Ou -a AD,DP --skip-indels \
+		-q ${q} -Q ${Q} -d ${d} -T "${tgt_file}" |
+		bcftools query \
+			-f "%CHROM\t%POS\t${SAMPLE}\t[%AD{0}\t%AD{1}]\n" \
+			-o "${tumor_1bed}" &>"${LOGDIR}/count.${SAMPLE}.${CHROM}.log" &
+
+    while [[ $(jobs -r -p | wc -l) -ge $MAXJOBS ]]; do
+        sleep 10
+    done
+done
+wait
+
+
+########################################
+echo "start phasing"
+for CHR in $CHROMS; do
+    CHROM=chr${CHR}
+    phase_file=${TMPDIR}/${CHROM}.phased.vcf.gz
+    snp_file="${snp_dir}/${CHROM}.vcf.gz"
+    if [[ -f ${phase_file} ]]; then
+        echo "${CHROM} exists"
+        continue
+    fi
+    hiphase \
+        --bam ${NORMAL_BAM} \
+        --vcf ${snp_file} \
+        --output-vcf ${phase_file} \
+        --reference ${reference} \
+        --threads 2 \
+        --ignore-read-groups &>"${LOGDIR}/hiphase.${CHROM}.log" &
+    
+    while [[ $(jobs -r -p | wc -l) -ge $MAXJOBS ]]; do
+        sleep 10
+    done
+done
+
+########################################
+echo "concat files"
+baf_dir="${OUTDIR}/baf"
+phase_dir="${OUTDIR}/phase"
+mkdir -p ${baf_dir}
+mkdir -p ${phase_dir}
+
+normal_1bed=${baf_dir}/normal.1bed
+tumor_1bed=${baf_dir}/tumor.1bed
+phase_list_file=${TMPDIR}/phase.list
+
+>${normal_1bed}
+>${tumor_1bed}
+>${phase_list_file}
+
+for CHR in $CHROMS; do
+    CHROM=chr${CHR}
+    ch_normal_1bed="${TMPDIR}/normal.${CHROM}.1bed"
+    ch_tumor_1bed="${TMPDIR}/${SAMPLE}.${CHROM}.1bed"
+    ch_phase_file=${phase_dir}/${CHROM}.phased.vcf.gz
+    cat ${ch_normal_1bed} >> ${normal_1bed}
+    cat ${ch_tumor_1bed} >> ${tumor_1bed}
+    bcftools index -f ${ch_phase_file}
+    echo "${ch_phase_file}" >> ${phase_list_file}
+done
+
+phase_file=${phase_dir}/phased.vcf.gz
+bcftools concat --file-list ${phase_list_file} -Ou \
+    | bcftools sort -Oz -o ${concat_vcf_file}
+bcftools index -f ${phase_file}
+
+echo "Done"
